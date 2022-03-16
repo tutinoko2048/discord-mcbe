a72aaa03f3235fabef2bc2e75164e4e649614c29
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2022-03-16 09:20:42 +0900

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,17 @@
+module.exports = 
+{
+  discord: { // discordに出力されるもの
+    ready: '[log] 起動しました',
+    connectionOpen: '[log] $1 : 接続を開始しました', // $1: 繋いだ人のプレイヤー名
+    connectionClose: '[log] 接続が終了しました',
+    chat: '[Minecraft] <$1> $2', // $1: プレイヤー名, $2: メッセージ
+    me: '[Minecraft] * $1 $2', // $1: プレイヤー名, $2 メッセージ
+    say: '[Minecraft] $1', // $1: プレイヤー名とメッセージ
+    list: '現在の人数: $1/$2\nプレイヤー:\n$3' // $1: 現在の人数, $2: 最大人数, $3: プレイヤー一覧
+  },
+  minecraft: { // Minecraftに出力されるもの
+    chat: '§b[Discord] $1 : $2', // $1: ユーザー名, $2: メッセージ
+    file: '§b[Discord] $1 : [$2 file]', // $1: ユーザー名, $2: ファイル拡張子
+    command: '§a[Discord] $1 : $2' // $1: ユーザー名, $2: コマンド
+  }
+}
