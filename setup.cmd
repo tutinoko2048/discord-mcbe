9841bee125c78b28a02dd2696dfccbf1be429036
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2022-04-10 17:53:16 +0900

    Add files via upload

---
+++ setup.cmd
@@ -0,0 +1,3 @@
+cd /d %~dp0
+echo installing packages...
+npm i
--- start.cmd
+++ start.cmd
@@ -1,5 +1,5 @@
+@echo off
 cd /d %~dp0
-
-if not exist "node_modules" call npm i
-
-call npm run start
+if not exist "node_modules" call setup.cmd
+echo starting...
+npm run start
