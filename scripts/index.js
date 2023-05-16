63ac0ea20d67e5f5b8e72b214a33133f72d8ee75
Retoruto9900K <tutinoko2048@gmail.com>
2023-05-16 03:03:01 +0000

    docs: other features

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ const PREFIX = '.';
 server.events.on('playerChat', async ev => {
   const { message, sender, world } = ev;
   
-  if (message.startsWith(PREFIX)) {
+  if (message.startsWith(PREFIX)) { // チャットの擬似コマンドのサンプル
     const [ command, ...args ] = message.slice(PREFIX.length).split(' ');
     
     if (command === 'help') {
