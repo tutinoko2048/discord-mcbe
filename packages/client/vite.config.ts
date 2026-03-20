1e01f7ffa002efaeca03d18cc0ac2197673396bd
RetoRuto9900K <tutinoko2048@gmail.com>
2026-03-20 23:34:20 +0900

    chore: prepare `vite.config.ts` for each packages

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from '@discord-mcbe/internal-config/vite';
+
+export default defineConfig({
+  fmt: {
+    ignorePatterns: [],
+  },
+});
--- projects/server/vite.config.ts
+++ projects/server/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from '@discord-mcbe/internal-config/vite';
 
 export default defineConfig({
   fmt: {
-    ignorePatterns: ['src/types/lang.generated.ts', 'schema.config.json'],
+    ignorePatterns: [
+      'src/types/lang.generated.ts',
+      'schema.config.json',
+      'src/types/config.ts',
+      'src/types/env.ts',
+    ],
   },
 });
