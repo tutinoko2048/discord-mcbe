cacfd1337419ccf9e1a683a56bda3d6ec7cec9de
RetoRuto9900K <tutinoko2048@gmail.com>
2026-03-21 02:32:27 +0900

    add: add script to bump package version

---
+++ scripts/version.ts
@@ -0,0 +1,79 @@
+import { $ } from 'bun';
+import { join } from 'path';
+
+const type = process.argv[2] as 'app' | 'launcher' | undefined;
+const version = process.argv[3];
+if (!type || !version || !['app', 'launcher'].includes(type)) {
+  console.error('Usage:\npnpm run version <app|launcher> <major|minor|patch|pre>\npnpm run version <app|launcher> <version>');
+  process.exit(1);
+}
+
+const isSpecified = /^\d+\.\d+\.\d+(-\w+\.\d+)?$/.test(version.trim());
+
+const packages = {
+  app: ['../projects/server', '../packages/client', '../packages/shared'],
+  launcher: ['../projects/launcher'],
+}
+
+const addons = ['../projects/addon-bds', '../projects/addon-local'];
+
+let updatedVersion: string;
+
+if (isSpecified) {
+  console.log(`Setting version to ${version}...`);
+
+  for (const pkg of packages[type]) {
+    await runPnpmVersion(join(__dirname, pkg));
+  }
+
+  updatedVersion = version.trim();
+} else {
+  console.log(`Bumping ${version} version...`);
+
+  for (const pkg of packages[type]) {
+    await runPnpmVersion(join(__dirname, pkg));
+  }
+
+  updatedVersion = await readCurrentVersion();
+}
+
+if (type === 'app') {
+  await updateAddonVersion(updatedVersion);
+}
+
+console.log(`Version updated to ${updatedVersion} successfully!`);
+
+
+async function runPnpmVersion(cwd: string) {
+  try {
+    await $`pnpm version ${version} --no-git-tag-version`.cwd(cwd).quiet();
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+
+  console.log(`- updated package.json: ${cwd}`);
+}
+
+async function readCurrentVersion() {
+  const packageJsonFile = Bun.file(join(__dirname, '../projects/server/package.json'));
+  const packageJson = await packageJsonFile.json();
+  return packageJson.version;
+}
+
+async function updateAddonVersion(version: string) {
+  for (const addonRelativePath of addons) {
+    const addonDir = join(__dirname, addonRelativePath);
+    const manifestPath = join(addonDir, 'manifest.json');
+    const manifestFile = Bun.file(manifestPath);
+
+    const manifest = await manifestFile.json();
+    manifest.header.version = version;
+    await manifestFile.write(JSON.stringify(manifest, null, 2));
+
+    await $`vp fmt 'manifest.json'`.cwd(addonDir).quiet();
+
+    console.log(`- updated manifest.json: ${addonDir}`);
+  }
+}
+
