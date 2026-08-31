104003886d41482b7aeaf6c747a52252215d2faf
RetoRuto9900K <tutinoko2048@gmail.com>
2026-09-01 05:25:55 +0900

    feat: support configuring addon via `variables.json`

--- packages/client/src/bds.ts
+++ packages/client/src/bds.ts
@@ -2,7 +2,9 @@ import { BaseClient, ClientType } from './client';
 import { ServerNetBridgeClient } from './transport/server-net';
 import type { ExtractOptional } from '@discord-mcbe/shared';
 import { world } from '@minecraft/server';
+import { variables } from '@minecraft/server-admin';
 import { handleClientBoundRequest } from './client/handler';
+import * as v from 'valibot';
 
 export interface BridgeClientOptions {
   host?: string;
@@ -10,10 +12,26 @@ export interface BridgeClientOptions {
   clientId?: string | (() => string);
 }
 
+const BdsVariablesSchema = v.object({
+  BRIDGE_HOST: v.fallback(v.pipe(v.string(), v.nonEmpty()), 'localhost'),
+  BRIDGE_PORT: v.fallback(v.pipe(v.number(), v.integer(), v.minValue(1)), 23191),
+  DEFAULT_CLIENT_ID: v.fallback(v.pipe(v.string(), v.nonEmpty()), 'discord-mcbe-bds'),
+});
+
+const vars = v.parse(BdsVariablesSchema, {
+  BRIDGE_HOST: variables.get('BRIDGE_HOST'),
+  BRIDGE_PORT: variables.get('BRIDGE_PORT'),
+  DEFAULT_CLIENT_ID: variables.get('DEFAULT_CLIENT_ID'),
+});
+
 const defaultOptions: ExtractOptional<BridgeClientOptions> = {
-  host: 'localhost',
-  port: 23191,
-  clientId: () => (world.getDynamicProperty('clientId') as string) ?? 'discord-mcbe-bds',
+  host: vars.BRIDGE_HOST,
+  port: vars.BRIDGE_PORT,
+  clientId: () => {
+    const clientId = world.getDynamicProperty('clientId');
+    if (typeof clientId === 'string') return clientId;
+    return vars.DEFAULT_CLIENT_ID;
+  },
 };
 
 export class BridgeClient extends BaseClient<ServerNetBridgeClient> {
